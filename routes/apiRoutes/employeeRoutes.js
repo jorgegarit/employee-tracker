69dd2c59eaf2739f444ded875e6fba4d2b5b9acb
Jorge Garit <jorgegarit@gmail.com>
2022-10-12 14:05:12 -0400

    finished remployee routes now to forcus on comand line index

--- routes/apiRoutes/employeeRoutes.js
+++ routes/apiRoutes/employeeRoutes.js
@@ -39,4 +39,60 @@ router.get("/employee/:id", (req, res) => {
     });
 });
 
+// create a new employee
+router.post('/employee', ({ body }, res) => {
+    // validation check
+    const errors = inputCheck(body, 'first_name', 'last_name', 'roles_id', 'manager_id');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO employees (first_name, last_name, roles_id, manager_id)
+        VALUES (?,?,?,?)`;
+    const params = [body.first_name, body.last_name, body.roles_id, body.manager_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+// update a current employee
+router.put('/employee/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'roles_id');
+
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    
+    const sql = `UPDATE employees SET roles_id = ?
+                WHERE id = ?`;
+    const params = [req.body.roles_id, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            // check if a record was found
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Employee not found'
+            });
+        } else {
+            res.json({ 
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
 module.exports = router;
