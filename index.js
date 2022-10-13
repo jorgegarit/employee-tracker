4c051801fa8f06afd645226529d2e5f1c5c7750c
Jorge Garit <jorgegarit@gmail.com>
2022-10-12 20:40:28 -0400

    added all functions pertaining to roles

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const initialize = () => {
         } else if (data.selections === 'Add New Department') {
             addDepartmentPrompt();
         } else if (data.selections === 'Add New Role') {
-            addRole();
+            addRolePrompt();
         } else if (data.selections === 'Add New Employee') {
             addEmployee();
         } else if (data.selections === 'Update Current Employee Role') {
@@ -111,3 +111,96 @@ const addDepartment = (body) => {
 // This section will contain the fucntion for getting all role data 
 // as well as prompt to add new role
 
+const allRoles = (req) => {
+    const sql = `SELECT * FROM roles`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        console.table(rows);
+        initialize();
+    });
+};
+
+// will prompt user for new role information
+const addRolePrompt = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of the new role?',
+            validate: inputtedTitle => {
+                if (inputtedTitle) {
+                    return true;
+                } else {
+                    console.log('Please enter a title for the role.');
+                    return false
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary for this role?',
+            validate: inputtedSalary => {
+                if (inputtedSalary) {
+                    return true;
+                } else {
+                    console.log('Please enter a salary for the role.');
+                    return false
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'departments',
+            message: 'Which department will this role be in?',
+            choices: [
+                {
+                    name: 'Products',
+                    value: 1
+                },
+                {
+                    name: 'Development',
+                    value: 2
+                },
+                {
+                    name: 'Ops',
+                    value: 3
+                },
+                {
+                    name: 'Sales',
+                    value: 4
+                },
+                {
+                    name: 'Finance',
+                    value: 5
+                }
+            ]
+        }
+    ])
+    .then(addRole);
+};
+
+const addRole = (body) => {
+    // validation check
+    const errors = inputCheck(body, 'title', 'salary', 'departments');
+    if (errors) {
+        console.log(errors);
+        return;
+    }
+    const sql = `INSERT INTO roles (title, salary, department_id)
+                VALUES (?,?,?)`;
+    const params = [body.title, body.salary, body.departments];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.table(result);
+        initialize();
+    });
+};
