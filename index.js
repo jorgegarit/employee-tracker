2f646885eceac2116fa5d65f3ffe4567e4686316
Jorge Garit <jorgegarit@gmail.com>
2022-10-12 20:09:24 -0400

    added the then data after each selection, need to create the function elements for each selection

--- index.js
+++ index.js
@@ -17,3 +17,48 @@ db.connect(error => {
     // initialize menu
     initialize();
 });
+
+// initialize application and display on console
+const initialize = () => {
+    inquirer.prompt([
+       {
+        type: 'list',
+        name: 'selections',
+        message: "Please select what you would like to do.",
+        choices: ['View Departments', 'View Roles', 'View Employees', 'Add New Department', 
+                    'Add New Role', 'Add New Employee', 'Update Current Employees Role', 'Quit']
+       } 
+    ])
+    .then(data => {
+        if (data.selections === 'View Departments') {
+            allDepartments();
+        } else if (data.selections === 'View Roles') {
+            allRoles();
+        } else if  (data.selections === 'View Employees') {
+            allEmployees();
+        } else if (data.selections === 'Add New Department') {
+            addDepartment();
+        } else if (data.selections === 'Add New Role') {
+            addRole();
+        } else if (data.selections === 'Add New Employee') {
+            addEmployee();
+        } else if (data.selections === 'Update Current Employee Role') {
+            updateEmployeeRole();
+        } else if (data.selections === 'Quit') {
+            quitSelections();
+        }
+    });
+};
+
+
+
+// // all departments will be displayed
+// const sql = `SELECT * FROM departments`;
+
+// db.query(sql, (err, rows) => {
+//     if (error) throw error;
+//     console.table(res);
+//     // return back to main menu
+//     initialize();
+    
+// });
